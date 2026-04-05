0df33b782e8fc76c43ec789ffc07677673b22191
georgemisaell <georgemisaelgantume@gmail.com>
2026-04-05 22:40:29 +0700

    fix: update BASE_URL for API endpoint

--- src/Api.jsx
+++ src/Api.jsx
@@ -1,6 +1,6 @@
 export const BASE_URL =
-  "https://script.google.com/macros/s/AKfycbwpEqKnQouDhcYQsQfJFVFjw486G3Ilxzi12cGpaKxsGmgR-_4iABEB6Y3AdiB2kZI2dg/exec";
-  
+  "https://script.google.com/macros/s/AKfycbxJormXSihwT-iFqMqpvb7kJrrGfB8fq__x9TTAf4QrTnEmuR0sJulHoWaMrlGk-h5P/exec";
+
 export async function checkIn(payload) {
   const response = await fetch(`${BASE_URL}?path=presence/checkin`, {
     method: "POST",
